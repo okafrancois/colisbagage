d90e5a8e4fd816d76d905b02001cbcc557d77f8f
Berny Itoutou <berny-francois@cosavostra.com>
2023-03-19 16:03:07 +0100

    feat(listable-fields): add slug to label classes

--- wp-content/plugins/classified-listing-pro/templates/listing/listable-fields.php
+++ wp-content/plugins/classified-listing-pro/templates/listing/listable-fields.php
@@ -32,7 +32,7 @@ if ( count( $fields ) ) :
                             echo $value . ' <span class="listable-value__unit">g max</span>';
                             break;
                         default:
-                            echo $value;
+                            echo $value . ' <span class="listable-value__unit">Kg</span>';
                     } ?>
                 </span>
             </div>
